6a418e82611abcf7e9dae215b27c0afd4148d8c6
Victor <vmcj2@ensign.edu>
2026-04-16 19:23:47 -0600

    Send Telegram alert when button is pressed

--- alert_button.py
+++ alert_button.py
@@ -5,8 +5,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+BOT_TOKEN = "8615568979:AAEpzTooWKtefSZSq34evHcj2e_SKm-uqv4"
+CHAT_ID = "8534410115"
 
 button_pressed = False
 
