ee28d7e5a44ff35f325ece49d94f4a9250df534d
Victor <vmcj2@ensign.edu>
2026-04-16 19:05:55 -0600

    init commit

--- alert_button.py
+++ alert_button.py
@@ -1,4 +1,30 @@
+
 import time
+import requests
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+BOT_TOKEN = "8615568979:AAEpzTooWKtefSZSq34evHcj2e_SKm-uqv4"
+CHAT_ID = "8534410115"
+
+button_pressed = False
+
+while True:
+    if GPIO.input(7) == GPIO.HIGH and not button_pressed:
+        print("Someone pressed the alert button!")
+        button_pressed = True
+
+        requests.post(
+            "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage",
+            json={"chat_id": CHAT_ID, "text": "Someone pressed the alert button!"}
+        )
+
+    elif GPIO.input(7) == GPIO.LOW:
+        button_pressed = False
+
+    time.sleep(0.1)import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
